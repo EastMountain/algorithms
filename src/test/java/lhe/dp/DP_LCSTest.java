b58e9b1e744ccc8fd565602762a024ecb251e6e3
EastMountain <helinxiang@gmail.com>
2014-09-16 18:37:43 +0800

    array

--- src/test/java/lhe/dp/DP_LCSTest.java
+++ src/test/java/lhe/dp/DP_LCSTest.java
@@ -5,14 +5,24 @@ import junit.framework.TestCase;
 public class DP_LCSTest  extends TestCase {
 
 	public void testDP_LCS(){
-		String xStr = "abct";
-		String yStr = "btc";
-    	String x[] = xStr.split(""); 
-    	String y[] = yStr.split(""); 
-    	DP_LCS lcs = new DP_LCS(x,y);
-    	lcs.buildOptimizeValue();
-    	lcs.buildOptimalSolution();
-    	lcs.print();
+//		String xStr = "abct";
+//		String yStr = "btc";
+//    	String x[] = xStr.split(""); 
+//    	String y[] = yStr.split(""); 
+//    	DP_LCS lcs = new DP_LCS(x,y);
+//    	lcs.buildOptimizeValue();
+//    	lcs.buildOptimalSolution();
+//    	lcs.print();
     	
+    	float fl = 111111111211111111121111111112111111111.1111111f;
+    	System.out.println(fl);
+    	
+    	int a=0,b=10,t=0;
+    	if(a==0)
+    		if(b>10)
+    			t++;
+    		else
+    			t--;
+    	System.out.println(t);
     }
 }
