92c8fb5a6e1ce59691f7d8a8b2c05f1d6f5776f0
EastMountain <helinxiang@gmail.com>
2014-09-30 17:40:37 +0800

    induction

---
+++ src/test/java/lhe/induction/MaxSubSegmentProductTest.java
@@ -0,0 +1,21 @@
+package lhe.induction;
+
+import junit.framework.TestCase;
+
+public class MaxSubSegmentProductTest extends TestCase {
+
+	public void testProcess(){
+		
+    	int a[] = {2,3,-1,2,-2,4,-8,0,10,10}; 
+//		int a[] = {2,3,0,-8,0,10}; 
+//		int a[] = {2,3,-1,2,-2,4};
+    	for(int i=0;i<a.length;i++){
+			System.out.print(a[i]+" ");
+		}
+    	System.out.println();
+    	MaxSubSegmentProduct mssp = new MaxSubSegmentProduct();
+    	
+    	System.out.println(mssp.process(a));
+    }
+
+}
